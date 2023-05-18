b912278e322f01c8c1ce42d26c19f18592462a6b
Devan Anjelito <anjelitodevan@gmail.com>
2023-05-18 20:44:41 +0700

    Add SMOTE.

--- src/config.py
+++ src/config.py
@@ -3,5 +3,8 @@ from pathlib import Path
 RANDOM_STATE = 8
 
 OG_SET = Path.cwd() / 'input/nasa.csv'
+
 TRAIN_SET = Path.cwd() / 'input/train.csv'
 TEST_SET = Path.cwd() / 'input/test.csv'
+
+SMOTE_TRAIN_SET = Path.cwd() / 'input/smote_train.csv'
--- src/train_test_creator.py
+++ src/train_test_creator.py
@@ -1,11 +1,16 @@
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
 
 import module
 import config
 
-if __name__ == "__main__":
+def create_train_test(
+    oversampling= False,
+    train_path= config.TRAIN_SET, 
+):
+
 
     df= pd.read_csv(config.OG_SET, usecols= module.USED_COLS)
 
@@ -24,16 +29,27 @@ if __name__ == "__main__":
         X, y, test_size= 0.2, stratify= y, shuffle= True, random_state= config.RANDOM_STATE
     )
 
+    if oversampling:
+        train_path= config.SMOTE_TRAIN_SET
+        sm = SMOTE(random_state= config.RANDOM_STATE)
+        X_train, y_train = sm.fit_resample(X_train, y_train)
+
     # concat X and y again
     train_set = pd.concat([X_train, y_train], axis=1)
     test_set = pd.concat([X_test, y_test], axis=1)
 
-    assert (train_set.shape[0] + test_set.shape[0]) == df_clean.shape[0]
+    if not oversampling:
+        assert (train_set.shape[0] + test_set.shape[0]) == df_clean.shape[0]
     assert train_set.shape[1] == df_clean.shape[1]
     assert test_set.shape[1] == df_clean.shape[1]
 
     # save df
-    train_set.to_csv(config.TRAIN_SET, index= False)
+    train_set.to_csv(train_path, index= False)
     test_set.to_csv(config.TEST_SET, index= False)
 
     print('Finish.')
+
+if __name__ == '__main__':
+    create_train_test(
+        oversampling= True
+    )
